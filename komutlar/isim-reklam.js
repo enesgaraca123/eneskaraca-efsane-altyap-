cf9a02ac2f352f153add0ef061146add6174081c
enesgaraca123 <75829045+enesgaraca123@users.noreply.github.com>
2021-01-27 09:40:25 +0300

    Update isim-reklam.js

--- komutlar/isim-reklam.js
+++ komutlar/isim-reklam.js
@@ -3,23 +3,23 @@ const ayarlar = require('../ayarlar.json');
 const db = require('quick.db');
 
 exports.run = async(client, message, args) => {
-let ingewikkeld = db.fetch("ce-nickreklam."+message.guild.id)
+let astralbeta = db.fetch("astral-nickreklam."+message.guild.id)
 let prefix = await require('quick.db').fetch(`prefix.${message.guild.id}`) || require('../ayarlar.json').prefix
 
-if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(new Discord.MessageEmbed() .setColor('RED') .setDescription(' Bunun için **UYELERI KICKLER** yetkisine codeming olmalısın. '))
-if(!message.guild.members.cache.get(client.user.id).hasPermission('KICK_MEMBERS')) return message.channel.send(new Discord.MessageEmbed() .setColor('RED') .setDescription(' Bunun için **UYELERI KICKLER** yetkisine codeming olmalısın. '))
+if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(new Discord.MessageEmbed() .setColor('RED') .setDescription(' Bunun için **UYELERI KICKLER** yetkisine olmalısın. '))
+if(!message.guild.members.cache.get(client.user.id).hasPermission('KICK_MEMBERS')) return message.channel.send(new Discord.MessageEmbed() .setColor('RED') .setDescription(' Bunun için **UYELERI KICKLER** yetkisine olmalısın. '))
   
 
-if(!ingewikkeld) {
-let codEmingMete = message.mentions.channels.first()
-if(!codEmingMete) return message.channel.send("Bir kanal pingle.")
-await db.set("ce-nickreklam."+message.guild.id, codEmingMete.id)
-return message.channel.send("CodEming olarak <#"+codEmingMete.id+"> kanalını log kanalı olarak ayarladık.")
+if(!astralbeta) {
+let astralcode = message.mentions.channels.first()
+if(!astralcode) return message.channel.send("Bir kanal pingle.")
+await db.set("astral-nickreklam."+message.guild.id, astralcode.id)
+return message.channel.send("AstralCode olarak <#"+astralcode.id+"> kanalını log kanalı olarak ayarladık.")
 }
 
-if(ingewikkeld) {
-await db.delete("ce-nickreklam."+message.guild.id)
-return message.channel.send(" Codeming olarak sıfırladık bu sıstemi ayarladık.")
+if(astralbeta) {
+await db.delete("astral-nickreklam."+message.guild.id)
+return message.channel.send(" AstralCode olarak sıfırladık bu sıstemi ayarladık.")
 }
 }
 exports.conf = {
