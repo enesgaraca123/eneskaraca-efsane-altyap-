6c46c47c8a4741c77b486d77b2f96c11d3640397
enesgaraca123 <75829045+enesgaraca123@users.noreply.github.com>
2021-01-27 09:31:03 +0300

    Update durum.js

--- komutlar/durum.js
+++ komutlar/durum.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
-const codeming = require('../ayarlar.json')
+const AstralCode = require('../ayarlar.json')
 exports.run = async (client, message, args) => {
-  client.user.setPresence({ activity: { name: `${codeming.prefix}yardım | ${client.guilds.cache.size} Sunucu | `}, status: 'dnd'})
+  client.user.setPresence({ activity: { name: `${AstralCode.prefix}yardım | ${client.guilds.cache.size} Sunucu | ${client.users.cache.size}Kullanıcı `}, status: 'dnd'})
   message.channel.send(new Discord.MessageEmbed().setColor('RANDOM').setDescription('İşlem Başarılı!'))
 }
 
