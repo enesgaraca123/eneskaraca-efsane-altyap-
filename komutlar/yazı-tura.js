13296ac2062fda0b34e6f8dcefde6be94f68ff86
enesgaraca123 <75829045+enesgaraca123@users.noreply.github.com>
2021-01-27 10:00:02 +0300

    Update yazı-tura.js

--- komutlar/yazı-tura.js
+++ komutlar/yazı-tura.js
@@ -6,31 +6,31 @@ const cevaplar = ["**YAZI-TURA: __TURA__**", "**YAZI-TURA: __YAZI__**"];
 
 exports.run = function(client, message) {
 
-var FwhyCode2 = cevaplar[Math.floor(Math.random() * cevaplar.length)];
+var AstralCode2 = cevaplar[Math.floor(Math.random() * cevaplar.length)];
 
-if (FwhyCode2 === "YAZI-TURA: __YAZI__**") {
+if (AstralCode2 === "YAZI-TURA: __YAZI__**") {
 
-const EmbedFwhyCode = new Discord.MessageEmbed()
+const EmbedAstralCode = new Discord.MessageEmbed()
 
       .setColor(0xf4b942)
-      .setDescription(FwhyCode2)
+      .setDescription(AstralCode2)
       .setThumbnail(
         "http://www.freakonomics.com/wp-content/uploads/2013/01/coin-300x218.jpg"
       );
 
-return message.channel.send(EmbedFwhyCode);
+return message.channel.send(EmbedAstralCode);
 
-  } else if (FwhyCode2 === "**YAZI-TURA: __TURA__**") {
+  } else if (AstralCode2 === "**YAZI-TURA: __TURA__**") {
 
-    const EmbedFwhyCode = new Discord.MessageEmbed()
+    const EmbedAstralCode = new Discord.MessageEmbed()
 
       .setColor(0xf4b942)
-      .setDescription(FwhyCode2)
+      .setDescription(AstralCode2)
       .setThumbnail(
         "https://www.wallets-online.com/webadmin/items/ogon/ogon_cd_red_1.jpg"
       );
 
-return message.channel.send(EmbedFwhyCode);
+return message.channel.send(EmbedAstralCode);
 
   }
 };
