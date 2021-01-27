ca424685c8f9d54caef2be1d2d77a23e73fc8b43
enesgaraca123 <75829045+enesgaraca123@users.noreply.github.com>
2021-01-27 09:35:09 +0300

    Update erken-hesap-engel.js

--- komutlar/erken-hesap-engel.js
+++ komutlar/erken-hesap-engel.js
@@ -6,21 +6,21 @@ exports.run = async(client, message, args) => {
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(":x: Bu komutu kullanmak için yeterli yetkiye sahip değilsin.");
   
   let kanal = message.mentions.channels.first()
-  let ana_veri = await db.fetch(`codeming_${message.guild.id}`)
+  let ana_veri = await db.fetch(`astralcode_${message.guild.id}`)
 
   
   if(ana_veri) {
     
 message.channel.send('Koruma sistemi ayarladığınız kanal verisi ile birlikte kaldırıldı.')  
     
-db.delete(`codeming_${message.guild.id}`)       
+db.delete(`astralcode_${message.guild.id}`)       
     return
   }
  if(!ana_veri) {
    
 if(!kanal)  return message.reply(':x: Ayarlama işlemini yapmam için bir kanal belirtin.')
 
-   db.set(`codeming_${message.guild.id}`, kanal.id)
+   db.set(`astralcode_${message.guild.id}`, kanal.id)
  
    //karrpuz: 🍉 
  message.channel.send('🍉 erken açılmış hesaplara ceza sistemi **aktif** hale getirildi. \n\n 🍉 Sunucuya gelen üyelerin hesapları en az **1 gün** olmalı. \n\n 🍉 Log kanalını '+kanal+' olarak ayarladım.')  
